d1bd611d75d92dbacb652e52879fc06e604f6104
Fritz1414213562 <fritz1414213562@gmail.com>
2020-05-16 18:22:35 +0900

    first commit: utility include file

---
+++ utility/utility.hpp
@@ -0,0 +1,7 @@
+#ifndef COFFEE_MAKERS_UTILITY_HPP
+#define COFFEE_MAKERS_UTILITY_HPP
+
+#include<coffee-makers/utility/inner_product.hpp>
+#include<coffee-makers/utility/distance.hpp>
+
+#endif // COFFEE_MAKERS_UTILITY_HPP
