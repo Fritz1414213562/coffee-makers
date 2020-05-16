1aa30c3f2f40454c681e98c1bfe68451a7dff165
Fritz1414213562 <fritz1414213562@gmail.com>
2020-05-16 18:20:06 +0900

    feat: add new header

--- CompileTimeCalculation/CompileTimeCalculation.hpp
+++ CompileTimeCalculation/CompileTimeCalculation.hpp
@@ -3,5 +3,7 @@
 #include<coffee-makers/CompileTimeCalculation/conjunction.hpp>
 #include<coffee-makers/CompileTimeCalculation/disjunction.hpp>
 #include<coffee-makers/CompileTimeCalculation/matrix_traits.hpp>
+#include<coffee-makers/CompileTimeCalculation/convertible.hpp>
+#include<coffee-makers/CompileTimeCalculation/return_type_traits.hpp>
 
 #endif /* COFFEE_MAKERS_COMPILE_TIME */
