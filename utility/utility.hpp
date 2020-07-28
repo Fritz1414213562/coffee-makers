b1410564d94ed234161a03cfb27dfd5176be52ab
Fritz1414213562 <fritz1414213562@gmail.com>
2020-07-28 13:38:01 +0900

    feat: add sum or sort header file

--- utility/utility.hpp
+++ utility/utility.hpp
@@ -3,5 +3,7 @@
 
 #include<coffee-makers/utility/inner_product.hpp>
 #include<coffee-makers/utility/distance.hpp>
+#include<coffee-makers/utility/sum.hpp>
+#include<coffee-makers/utility/sort.hpp>
 
 #endif // COFFEE_MAKERS_UTILITY_HPP
