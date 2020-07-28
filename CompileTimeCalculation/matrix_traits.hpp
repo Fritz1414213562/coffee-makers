6f73436ecde853147602ed74a54d33dcbd9df11b
Fritz1414213562 <fritz1414213562@gmail.com>
2020-07-28 13:38:24 +0900

    feat: is_fixed, is_variable methods are added

--- CompileTimeCalculation/matrix_traits.hpp
+++ CompileTimeCalculation/matrix_traits.hpp
@@ -12,6 +12,10 @@ using is_variable = value_disjunction<
 	(MatrixType::Row_CompileTime < 0), (MatrixType::Col_CompileTime < 0)>;
 
 template<typename MatrixType>
+using is_fixed = value_conjunction<
+	(MatrixType::Row_CompileTime > 0), (MatrixType::Col_CompileTime > 0)>;
+
+template<typename MatrixType>
 using is_square_matrix = std::bool_constant<
 	(MatrixType::Row_CompileTime == MatrixType::Col_CompileTime)>;
 
